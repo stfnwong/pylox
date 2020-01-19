f4884d31d71faf244022a2c0bbd7f6fb873e27ab
Stefan Wong <stfnwong@gmail.com>
2020-01-19 21:56:35 +0000

    Some cleanup in test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -22,36 +22,28 @@ class TestParser(unittest.TestCase):
     def setUp(self) -> None:
         self.verbose = True
 
-    def test_simple_expr(self) -> None:
+    def test_simple_add(self) -> None:
         # Input expression
         simple_expr = '2 + 2;'
-
         # Format the expected output
         token_left    = Token.Token(Token.NUMBER, '2', float(2), 1)
         token_op      = Token.Token(Token.PLUS, '', None, 1)
         token_right   = Token.Token(Token.NUMBER, '2', float(2), 1)
-        exp_output  = Expression.Binary(token_left, token_op, token_right)
+        exp_output    = Expression.Binary(token_left, token_op, token_right)
 
-        scanner    = Scanner.Scanner(simple_expr)
-        token_list = scanner.scan()
-        parser     = Parser.Parser(token_list)
-
-        # Show in console what the parsed output was
-        print(parser)
+        # Get some objects to test
+        scanner       = Scanner.Scanner(simple_expr)
+        token_list    = scanner.scan()
+        parser        = Parser.Parser(token_list)
         parsed_output = parser.parse()
-        print(type(parsed_output))
-        print(parsed_output)
-
-        exp_left  = Expression.Literal(token_left)
-        exp_right = Expression.Literal(token_right)
 
         # Now check that the output matches
+        exp_left  = Expression.Literal(token_left)
+        exp_right = Expression.Literal(token_right)
         self.assertEqual(exp_left, parsed_output.left)
         self.assertEqual(exp_right, parsed_output.right)
         self.assertEqual(token_op, parsed_output.op)
 
 
-
 if __name__ == '__main__':
     unittest.main()
-
